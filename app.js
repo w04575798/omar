c8a71647843400f35cd3891b0f358a5e7146a4e1
Omar <w0475798@campus.nscc.ca>
2023-12-08 14:14:01 -0400

    new

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ const usersRouter = require('./routes/api/users');
 // define the app
 var app = express();
 
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
